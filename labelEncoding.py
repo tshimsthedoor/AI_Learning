208d88f1d3e10f06cda29da5eae455b981cf34f3
tresor <tshims79@gmail.com>
2023-06-22 23:44:45 +0200

    Printing the mapping between words and numbers

--- labelEncoding.py
+++ labelEncoding.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import preprocessing
 
 # Sample input labels
-input_labels = ['red', 'balck', 'red', 'green', 'black', 'yellow', 'white']
+input_labels = ["red", "balck", "red", "green", "black", "yellow", "white"]
 
 # Create label encoder and fit the labels
 encoder = preprocessing.LabelEncoder()
@@ -11,4 +11,10 @@ encoder.fit(input_labels)
 # Print the mapping
 print("\nLabel mapping:")
 for i, item in enumerate(encoder.classes_):
-    print(item, '-->', i)
+    print(item, "-->", i)
+
+# Encode a set of labels using the encoder
+test_labels = ["green", "red", "black"]
+encoded_values = encoder.transform(test_labels)
+print("\nLabels =", test_labels)
+print("Encoded values =", list(encoded_values))
