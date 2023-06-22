I fixed the black on Python with normalization

first.py
@@ -1,7 +1,9 @@
 import numpy as np
 from sklearn import preprocessing
 
-input_data = np.array([[5.1, -2.9, 3.3], [-1.2, 7.8, -6.1], [3.9, 0.4, 2.1], [7.3, -9.9, -4.5]])
+input_data = np.array(
+    [[5.1, -2.9, 3.3], [-1.2, 7.8, -6.1], [3.9, 0.4, 2.1], [7.3, -9.9, -4.5]]
+)
 # Binarize data
 data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
 print("\nBinarized data:\n", data_binarized)
@@ -16,3 +18,8 @@ data_scaled = preprocessing.scale(input_data)
 print("\nAFTER:")
 print("Mean =", data_scaled.mean(axis=0))
 print("Std deviation =", data_scaled.std(axis=0))
+
+# Normalize data
+data_normalized_l1 = preprocessing.normalize(input_data, norm='l1')
+data_normalized_l2 = preprocessing.normalize(input_data, norm='l2')
+print("\nL1 normalized data:\n", data_normalized_l1)