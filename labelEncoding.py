64fe2e05fb38ed4d32d6ff49c0d82414f70351ce
tresor <tshims79@gmail.com>
2023-06-22 23:58:32 +0200

    Let's decode a random set of numbers

--- labelEncoding.py
+++ labelEncoding.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import preprocessing
 
 # Sample input labels
-input_labels = ["red", "balck", "red", "green", "black", "yellow", "white"]
+input_labels = ["red", "black", "red", "green", "black", "yellow", "white"]
 
 # Create label encoder and fit the labels
 encoder = preprocessing.LabelEncoder()
@@ -18,3 +18,9 @@ test_labels = ["green", "red", "black"]
 encoded_values = encoder.transform(test_labels)
 print("\nLabels =", test_labels)
 print("Encoded values =", list(encoded_values))
+
+# Decode a set of values using the encoder
+encoded_values = [3, 0, 4, 1]
+decoded_list = encoder.inverse_transform(encoded_values)
+print("\nEncoded values =", encoded_values)
+print("Decoded labels =", list(decoded_list))
